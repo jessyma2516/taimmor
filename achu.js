35cb1db08846818c56629976f1c05756011ae70b
jessyma2516 <jessymamsj@gmail.com>
2025-04-21 17:06:35 +0530

    Update achu.js

--- achu.js
+++ achu.js
@@ -1,6 +1,4 @@
 console.log("hello world");
-<<<<<<< HEAD
-console.log("i am 4 years old");
-=======
+
+console.log("i am 4 years old")
 console.log("hello i am your son")
->>>>>>> afd83944ccecfff9686fe4c73a8c267b173946f9
