ea56a053739d7d5e7159b44c33eca26b40571c90
jessyma2516 <jessymamsj@gmail.com>
2025-04-21 16:44:55 +0530

    file added

---
+++ taimoor2.js
@@ -0,0 +1,13 @@
+ // var- we can reassign and redeclared the values
+ var a=10;
+ var a=20;
+ 
+ console.log(a);
+ 
+// let -we can reassign the value but we cant redeclare the values
+ let b=20;
+ b=10;
+ console.log(b);
+ // cont we can assign value only
+ const c=50;
+ console.log(c);
